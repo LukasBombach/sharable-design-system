f9da6c77abddea940ecfa1acad7989e0770a389c
Lukas Bombach <lukas.bombach@googlemail.com>
2021-11-07 15:51:57 +0100

    qip

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,33 @@
+import type { VFC, CSSProperties } from "react";
 import type { NextPage } from "next";
+
+interface Stylable {
+  className?: string;
+  style?: CSSProperties;
+}
+
+const Placeholder: VFC<Stylable> = ({ className, style }) => (
+  <div
+    className={`h:center v:center ${className}`}
+    style={{ height: 200, background: "lightgrey", ...style }}
+  >
+    Placeholder
+  </div>
+);
 
 const Home: NextPage = () => {
   return (
     <div className="w:page h:center">
-      <div>x</div>
+      <div className="layout:companion">
+        <Placeholder
+          className="layout:place"
+          style={{ "--placement": "companion" } as CSSProperties}
+        />
+        <Placeholder
+          className="layout:place"
+          style={{ "--placement": "content" } as CSSProperties}
+        />
+      </div>
     </div>
   );
 };
